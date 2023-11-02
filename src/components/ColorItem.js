dc0f902e999d2fb240205f7693504b437cab2fdc
livotieno <livio5261@gmail.com>
2023-11-02 09:14:03 +0300

    done

---
+++ src/components/ColorItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColorItem = ({ color }) => {
+  return <li style={{ color: color }}>{color}</li>;
+};
+
+export default ColorItem;
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorItem from "./ColorItem";
 
 function ColorList() {
   const colors = [
@@ -8,7 +9,6 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
@@ -19,8 +19,15 @@ function ColorList() {
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
       </ol>
+
+      {/* list and keys */}
+      <ol>
+        {colors.map((color, index) => {
+          return <ColorItem key={index} color={color} />;
+        })}
+      </ol>
     </div>
   );
 }
-
 export default ColorList;
+
